f6fd01672766d5e34b06ea549c33410f1dce1973
Win Barua <win.barua2@gmail.com>
2022-10-12 21:17:56 +0100

    Completed home page functionality

--- client/src/components/homepage-player/HomepagePlayer.js
+++ client/src/components/homepage-player/HomepagePlayer.js
@@ -1,11 +1,17 @@
 import './homepage-player.css';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import axios from 'axios';
+import infoIcon from 'assets/icons/info.png';
+import playIcon from 'assets/icons/play-icon.png';
+import pauseIcon from 'assets/icons/pause-icon.png';
 
 const HomepagePlayer = () => {
   const [songTitle, setSongTitle] = useState('');
   const [songArtist, setSongArtist] = useState('');
   const [songCover, setSongCover] = useState('');
+  const [audioSrc, setAudioSrc] = useState('');
+  const [songUrl, setSongUrl] = useState('');
+  const progressContainer = useRef(null);
 
   useEffect(() => {
     fetchDailySong();
@@ -21,7 +27,38 @@ const HomepagePlayer = () => {
         setSongCover(
           response.data['track']['track']['album']['images'][0]['url']
         );
+        setAudioSrc(response.data['track']['track']['preview_url']);
+        setSongUrl(response.data['track']['track']['external_urls']['spotify']);
       });
+  };
+
+  const [playState, setPlayState] = useState(false);
+  const playSong = () => {
+    document.querySelector('#audio').play();
+    setPlayState(true);
+  };
+
+  const pauseSong = () => {
+    document.querySelector('#audio').pause();
+    setPlayState(false);
+  };
+
+  // Update progress visual every second
+  const updateProgress = (e) => {
+    const progress = document.querySelector('.progress');
+    const { duration, currentTime } = e.target;
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`;
+  };
+
+  // Set progress on visual and audio when user clicks on the progress bar
+  const setProgress = (e) => {
+    const width = progressContainer.current.clientWidth;
+    const clickX = e.clientX - e.target.offsetLeft;
+    const audio = document.querySelector('#audio');
+    const duration = audio.duration;
+
+    audio.currentTime = (clickX / width) * duration;
   };
 
   return (
@@ -32,6 +69,39 @@ const HomepagePlayer = () => {
         <p>{songArtist}</p>
       </div>
       <img id="songCover" src={songCover} alt="" />
+      <audio src={audioSrc} id="audio" onTimeUpdate={updateProgress}></audio>
+
+      <div className="media-controls">
+        {playState ? (
+          <button className="media-btn" onClick={pauseSong}>
+            <img src={pauseIcon} alt="pause" />
+          </button>
+        ) : (
+          <button className="media-btn" onClick={playSong}>
+            <img src={playIcon} alt="play" />
+          </button>
+        )}
+
+        <div className="progressAndLink"></div>
+        <div
+          ref={progressContainer}
+          className="progress-container"
+          onClick={setProgress}
+        >
+          <div className="progress"></div>
+        </div>
+
+        <div className="linksContainer">
+          <a
+            href={songUrl}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="deezer-btn"
+          >
+            Full song on Spotify
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
