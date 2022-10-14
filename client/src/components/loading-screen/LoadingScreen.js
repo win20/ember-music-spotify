214586e95ec66463ec835d49e3b01ee2d2205747
Win Barua <win.barua2@gmail.com>
2022-10-14 18:48:27 +0100

    Cleaned up errors and warnings

--- client/src/components/loading-screen/LoadingScreen.js
+++ client/src/components/loading-screen/LoadingScreen.js
@@ -1,11 +1,9 @@
-import loadingIcon from 'assets/icons/play-icon.png';
 import './loading-screen.css';
-import { useState, useEffect } from 'react';
 
 const LoadingScreen = () => {
   return (
     <div id="LoadingScreen" className="startAnimation">
-      <div class="lds-facebook">
+      <div className="lds-facebook">
         <div></div>
         <div></div>
         <div></div>
