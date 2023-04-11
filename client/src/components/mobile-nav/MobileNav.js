54577a09e1bad39c5ac588f31a6dc2448eded2c8
win20 <win.barua2@gmail.com>
2023-04-11 18:58:06 +0100

    add home link to mobile nav

--- client/src/components/mobile-nav/MobileNav.js
+++ client/src/components/mobile-nav/MobileNav.js
@@ -10,6 +10,7 @@ const MobileNav = () => {
     <div id="MobileNav">
       <img id="mobile-nav-close-btn" src={closeIcon} onClick={handleClose} />
       <div id="mobile-nav-items">
+        <a href="/">Home</a>
         <a href="/recommendations">Recommendations</a>
         <a href="/studyMode">Study mode</a>
       </div>
